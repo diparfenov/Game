d962204fa2e49a3644cafe7c08d2af66c32bc893
DMITRII <119112579+diparfenov@users.noreply.github.com>
2023-04-25 01:31:20 +0400

    Update Header.js

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-      <p>-Rock-Scissors-Paper-</p>
+      <p>-Rock-Paper-Scissors-</p>
     </div>
   );
 }
